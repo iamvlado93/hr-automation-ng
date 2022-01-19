e76199012dbf3ac79f11a4c092804568d271958a
iamvlado93 <vladrjeousski@gmail.com>
2022-01-19 17:52:15 +0300

    feat: fixed path for json folder

--- hr-automation/src/app/components/github/github.component.ts
+++ hr-automation/src/app/components/github/github.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import githubUsers from '../jsonFolder/users.json';
+import githubUsers from './jsonFolder/users.json';
 
 interface GithubUser {
   login: string;
